4ca06a35ad808a86872497af260da23a95d324f0
Guilherme <gg-lima@outlook.com>
2020-10-25 03:16:11 -0400

    Update the KivaController
    Creating Kiva and Terrain attributes.
    Passing the KivaController instead of Kiva.

--- src/kiva_robot/kiva_controller.py
+++ src/kiva_robot/kiva_controller.py
@@ -9,9 +9,24 @@ from kiva_robot.kiva_command import Commands
 class KivaController():
     ''' This interface implements Kiva Robot's controller
     '''
-    def __init__(self, kiva: 'Kiva') -> None:
+    def __init__(self, kiva: 'Kiva', terrain: 'str') -> None:
         self.__kiva = kiva
+        self.__terrain = terrain
+
+    # Getters
+
+    @property
+    def kiva(self) -> 'Kiva':
+        return self.__kiva
+
+    @property
+    def terrain(self) -> 'str':
+        return self.__terrain
+
+    # Methods
 
     def update(self, input: 'Commands') -> None:
+        ''' Execute Kiva Robot's command.
+        '''
         # print(f"Updating kiva: {input.code}")
-        input.command.execute(self.__kiva)
+        input.command.execute(self)
